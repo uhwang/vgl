4044b48f892019e3106a8757fd4ad09e0b2de5c1
uhwang <43251090+uhwang@users.noreply.github.com>
2020-04-14 11:40:52 -0600

    Add files via upload

---
+++ vgl/mesh3d.py
@@ -0,0 +1,101 @@
+# face3d.py
+
+import numpy as np
+
+MESH_WIREFRAME = 0
+MESH_HIDDENLINE = 1
+
+class Node3d():
+    def __init__(self, coord=(0,0,0)):
+        self.x = coord[0]
+        self.y = coord[1]
+        self.z = coord[2]
+		
+class Face3d():
+	def __init__(self, node_index=[], avgz=0):
+		self.index = node_index
+		self.avgz = avgz
+	
+# p1, p2 are node indices
+class Edge3d():
+	def __init__(self, i1=0, i2=0):
+		self.node1 = i1
+		self.node2 = i2
+		
+class Mesh3d():
+	def __init__(self, jpnt, ipnt):
+		self.mode = MESH_WIREFRAME
+		self.jpnt = jpnt
+		self.ipnt = ipnt
+
+		nface = (jpnt-1)*(ipnt-1)
+		self.node = []#[Node3d()]*nnode
+		self.face = []#[Face3d()]*nface
+		self.edge = []#[Edge3d()]*nface
+		self.tnode = []
+		
+	def create_tansform_node(self, v3d):
+		self.tnode = []
+		self.zvalue = []
+		nnode = self.jpnt*self.ipnt
+		for n in self.node:
+			p = v3d.rotate_point([n.x,n.y,n.z])
+			self.zvalue.append(p[2])
+			self.tnode.append((p[0],p[1]))
+		
+	def compute_ave_z(self):
+		self.avg_z = []
+		for face in self.face:
+			n = self.zvalue
+			z = (n[face.index[0]] + n[face.index[1]] +
+				 n[face.index[2]] + n[face.index[3]]) / 4.0
+			self.avg_z.append(z)
+			
+	def create_node(self, geom):
+		for j in range(self.jpnt):
+			jjpos = j * self.jpnt
+			for i in range(self.ipnt):
+				npos = jjpos+i
+				#pnt = self.node[npos]
+				#self.node[npos].x = geom[j][i][0]
+				#self.node[npos].y = geom[j][i][1]
+				#self.node[npos].z = geom[j][i][2]
+				#pnt.x = geom[j][i][0]
+				#pnt.y = geom[j][i][1]
+				#pnt.z = geom[j][i][2]
+				#print(geom[j][i])
+				#print(pnt.x,pnt.y,pnt.z)
+				#print(self.node[npos].x, self.node[npos].y, self.node[npos].z)
+				gp = geom[j][i]
+				self.node.append(Node3d((gp[0],gp[1],gp[2])))
+				
+	def create_mesh(self):
+		jpan = self.jpnt-1
+		ipan = self.ipnt-1
+		jpnt = self.jpnt
+		ipnt = self.ipnt
+		for j in range(jpan):
+			jjpnt = j*jpnt
+			for i in range(ipan):
+				a = jjpnt + i  
+				b = jjpnt + (i+1)
+				c = (j+1)*jpnt+(i+1)
+				d = (j+1)*jpnt+i
+				#self.face[jjpan+i].index = [a,b,c,d]
+				#self.face[jjpan+i].avgz = (self.node[a].z+self.node[b].z+
+				#                           self.node[c].z+self.node[d].z)/4.0
+				#self.edge[jjpan+i].node1 = a
+				#self.edge[jjpan+i].node2 = b
+				#print(a,b,c,d)
+				avgz = (self.node[a].z+self.node[b].z+self.node[c].z+self.node[d].z)/4.0
+				self.face.append(Face3d([a,b,c,d], avgz ))
+				self.edge.append(Edge3d(a,b))
+				self.edge.append(Edge3d(a,d))
+			self.edge.append(Edge3d(b,c))
+			
+		jjpnt += ipnt
+		for i in range(ipan):
+			a = jjpnt + i
+			b = jjpnt + (i+1)
+			self.edge.append(Edge3d(a,b))
+			
--- vgl/plot.py
+++ vgl/plot.py
@@ -7,3 +7,43 @@
 # Author: Uisang Hwang
 # Email : uhwangtx@gmail.com
 #
+
+import view3d
+from vgl import color
+import mesh3d
+import numpy as np
+from operator import itemgetter
+
+def convertToComputerFrame(point):
+	computerFrameChangeMatrix = np.array([[-1, 0, 0], [0, 0, -1], [0, -1, 0]])
+	return np.matmul(computerFrameChangeMatrix, point)
+	
+def plot_mesh(dev, mesh, v3d):
+	
+	xmin = dev.frm.data.xmin
+	xmax = dev.frm.data.xmax
+	ymin = dev.frm.data.ymin
+	ymax = dev.frm.data.ymax
+	xcen = xmin+(xmax-xmin)*0.5
+	ycen = ymin+(ymax-ymin)*0.5
+	
+	if mesh.mode is mesh3d.MESH_WIREFRAME:
+		for e1 in mesh.edge:
+			p0 = mesh.node[e1.node1]
+			q0 = mesh.node[e1.node2]
+			p1 = v3d.rotate_point([p0.x, p0.y, p0.z])
+			q1 = v3d.rotate_point([q0.x, q0.y, q0.z])
+			dev.line(p1[0], p1[1], q1[0], q1[1], color.BLACK, 0.001)
+
+	elif mesh.mode is mesh3d.MESH_HIDDENLINE:
+		for idx, val in sorted(enumerate(mesh.avg_z), key=itemgetter(1)):
+			face = mesh.face[idx]
+			p = [mesh.tnode[face.index[0]],
+				    mesh.tnode[face.index[1]],
+					mesh.tnode[face.index[2]],
+					mesh.tnode[face.index[3]]]
+			x = (p[0][0], p[1][0], p[2][0], p[3][0])
+			y = (p[0][1], p[1][1], p[2][1], p[3][1])
+			dev.polygon(x, y, color.BLACK, color.WHITE, lthk=0.001)
+			
+	#elif mode = MESH_HIDDENLINE:
