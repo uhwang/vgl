d595211f9f7d47cb5d12013509b05c7b83092760
uhwang <43251090+uhwang@users.noreply.github.com>
2020-04-16 07:09:24 -0600

    Add files via upload

--- samples/test_mesh.py
+++ samples/test_mesh.py
@@ -5,12 +5,9 @@ import numpy as np
 from vgl import color, geom, BBox, Frame, FrameManager, Data
 from vgl import DeviceWindowsMetafile, DeviceCairo, DevicePygame
 from vgl import drawfrm, drawtick, drawaxis, drawlabel
-
-import naca45
-import mesh3d
-import plot
-import view3d
+from vgl import view3d, mesh3d, plot
 import math
+import cylinder
 
 def create_sphere(r, jpan, ipan):
 	jpnt = jpan+1
@@ -22,9 +19,10 @@ def create_sphere(r, jpan, ipan):
 	geom = np.zeros((jpnt,ipnt,3),dtype=np.float32)
 	ang = 0
 	for j in range(jpnt):
-		start = pi_2 if math.fabs(ang-twopi) < 0.001 else 0
-		phi = start+j * dphi
-		ang = phi
+		#start = pi_2 if math.fabs(ang-twopi) < 0.001 else 0
+		#phi = start+j * dphi
+		#ang = phi
+		phi = j*dphi
 		for i in range(ipnt):
 			the = i*dthe
 			x = r*np.cos(phi)*np.cos(the)
@@ -33,54 +31,16 @@ def create_sphere(r, jpan, ipan):
 			geom[j][i][0] = x
 			geom[j][i][1] = y
 			geom[j][i][2] = z
-	print(geom)
 	return geom
 	
-def create_cylinder(r, jpan, ipan, zmin, zmax):
-	jpnt = jpan+1
-	ipnt = ipan+1
-	twopi= 2*np.pi
-	dthe = twopi/(ipan)
-	geom = np.zeros((jpnt,ipnt,3),dtype=np.float32)
-	dz = (zmax-zmin)/jpan
-
-	for j in range(jpnt):
-		z = zmin + j*dz
-		for i in range(ipnt):
-			the = i * dthe
-			x = r*np.cos(the)
-			y = r*np.sin(the)
-			geom[j][i][0] = x
-			geom[j][i][1] = y
-			geom[j][i][2] = z
-			
-	#for i in range(ipnt):
-	#	the = i * dthe
-	#	x = r*np.cos(the)
-	#	y = r*np.sin(the)
-	#	geom[0][i][0] = x
-	#	geom[0][i][1] = y
-	#	geom[0][i][2] = zmin
-	#
-	#for j in range(1,jpnt):
-	#	z = zmin+j * dz
-	#	geom[j,:,0:1] = geom[0,:,0:1]
-	#	#geom[j,:,1] = geom[0,:,1]
-	#	geom[j,:,2] = z
-	
-	return geom
-
-jpan = 10
+jpan = 15
 ipan = 10
 zmin = -0.4
 zmax = 0.4
 
 #geom = naca45.create_3d_wing("4412", jpan, ipan, 0, -0.5, 0.5)
 #geom = create_sphere(1, jpan, ipan)
-geom = create_cylinder(0.5, jpan, ipan, zmin, zmax)
-shape = geom.shape
-
-print(shape)
+geom = cylinder.create_cylinder(0.5, jpan, ipan, zmin, zmax)
 
 #=====================================
 
@@ -95,8 +55,8 @@ def compare_matplot():
 	
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
-	surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-	fig.colorbar(surf, shrink=0.5, aspect=5)
+	#surf = ax.plot_surface(X, Y, Z, linewidth=1, color = "#ff1010", antialiased=False)
+	surf = ax.plot_wireframe(X, Y, Z, linewidth=1, color = "#ff1010", antialiased=False)
 	plt.show()
 
 #=====================================
@@ -106,11 +66,14 @@ xmax = np.max(geom[:,:,0])
 ymin = np.min(geom[:,:,1])
 ymax = np.max(geom[:,:,1])
 
+# adjust ymin, ymax for wing geom
+#ymin =-0.5
+#ymax = 0.5
 data = Data(xmin, xmax, ymin, ymax, zmin, zmax)
 fmm = FrameManager()
 frm = fmm.create(0,0,5,5,data)
 
-mesh = mesh3d.Mesh3d(shape[0], shape[1])
+mesh = mesh3d.SquareMesh3d(geom.shape[0], geom.shape[1])
 mesh.create_node(geom)
 mesh.create_mesh()
 v3d = view3d.View3d(frm)
@@ -128,16 +91,18 @@ dev_rst = DevicePygame(gbbox, 100)
 buttons = (False,False,False)
 lbutton = False
 move = False
-scale = 1.0
 prv_mode = mesh3d.MESH_WIREFRAME
-
+win_size = dev_rst.size()
+trans = False
+trans_dx = data.get_xrange()*0.05
+trans_dy = data.get_yrange()*0.05
+scale = 1.0
 dev_rst.set_plot(frm)
 v3d.scaling(0.5)
 
 def plot_geom(dev):
-	drawaxis.draw_axis(dev)
-	drawtick.draw_tick_2d(dev)	
-	drawlabel.draw_label_2d(dev)
+	clip = dev.frm.get_clip()
+	dev.create_clip(clip[0],clip[1],clip[2],clip[3])
 	plot.plot_mesh(dev, mesh, v3d)
 	
 def save_cairo(fname, frm, gbox, dpi):
@@ -159,7 +124,7 @@ mesh.create_tansform_node(v3d)
 mesh.compute_ave_z()
 plot_geom(dev_rst)
 dev_rst.show()
-		
+
 while running:
 	dev_rst.fill_white()
 	loopRate = 50
@@ -174,18 +139,21 @@ while running:
 			if event.key == K_i:
 				prv_choice = choice
 				choice='i'
-			if event.key == K_m:
+			elif event.key == K_m:
 				prv_choice = choice
 				choice='m'
-				
 			elif event.key == K_w:
+				print("... Wireframe mode")
 				mesh.mode = mesh3d.MESH_WIREFRAME
+			elif event.key == K_t:
+				print("... translation")
+				trans = ~trans
+				print(trans)
 			elif event.key == K_h:
+				print("... Hiddenline mode")
 				mesh.mode = mesh3d.MESH_HIDDENLINE
-				mesh.create_tansform_node(v3d)
 				plot_geom(dev_rst)
 				dev_rst.show()
-			mesh.compute_ave_z()
 				
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			prv_mode = mesh.mode
@@ -201,11 +169,17 @@ while running:
 				#m_xRotate += old_pos[1]-new_pos[1]
 				dx = new_pos[0]-old_pos[0]
 				dy = new_pos[1]-old_pos[1]
-				m_xRotate += dy
-				m_yRotate += dx
-				v3d.xrotation(m_xRotate)
-				v3d.yrotation(m_yRotate)
-				old_pos = event.pos
+				if trans:
+					print("... Trans(dx: %d, dy: %d)"%(dx,dy))
+					tqx = trans_dx if dx < 0 else -trans_dx
+					tqy = trans_dy if dy > 0 else -trans_dy
+					frm.translate_xy(tqx, tqy)
+					dev_rst.set_plot(frm)
+				else:
+					m_xRotate += dy
+					m_yRotate += dx
+					v3d.xrotation(m_xRotate)
+					v3d.yrotation(m_yRotate)
 			
 			elif buttons[2]:
 				diffy = old_pos[1]-new_pos[1]
@@ -214,15 +188,19 @@ while running:
 					if diffy>0: scale -= 0.1
 					v3d.scaling(scale)
 					print('scaling.. ', diffy, scale)
+			old_pos = event.pos
+			
+			if buttons[0] or buttons[2]:
+				mesh.create_tansform_node(v3d)
 				
 		elif event.type == pygame.MOUSEBUTTONUP:
-			mesh.create_tansform_node(v3d)
-			mesh.compute_ave_z()
 			buttons = pygame.mouse.get_pressed()
 			lbutton = False
 			move = False
+			#trans = False
 			mesh.mode = prv_mode
 			if mesh.mode is mesh3d.MESH_HIDDENLINE:
+				mesh.compute_ave_z()
 				plot_geom(dev_rst)
 				dev_rst.show()
 			
