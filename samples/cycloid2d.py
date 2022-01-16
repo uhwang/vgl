ae8ec68c17c9f0f364a7442ba348a722f475029a
uhwang <43251090+uhwang@users.noreply.github.com>
2022-01-15 22:34:41 -0300

    Upgrade

--- samples/cycloid2d.py
+++ samples/cycloid2d.py
@@ -25,7 +25,7 @@ dur = 20
 fps = 20
 
 r1 = 1
-max_freq  = 1.5 # 2 Hz
+max_freq  = 3 # 2 Hz
 t1 = 0
 t2 = 2*np.pi*max_freq
 dt = (t2-t1)/(dur*fps)
@@ -47,6 +47,7 @@ def movie_cycloid(t):
     global dev, cycloid_trail_x, cycloid_trail_y
     dev.fill_white()
     vgl.drawtick.draw_tick_2d(dev)	
+    vgl.draw_frame(dev, frm)
     vgl.drawfrm.draw_grid(dev, dev.frm.data, 1)
     vgl.drawlabel.draw_label_2d(dev)
     
@@ -72,27 +73,37 @@ def plot_cycloid(dev):
     vgl.drawlabel.draw_label_2d(dev)
     dev.polyline(x,y,vgl.color.BLUE, dev.frm.hgt()*0.005)
     
-    
 import vgl
-xmin,xmax,ymin,ymax=0,10,-5,5
+xmin,xmax,ymin,ymax=0,20,-2,5
 data = vgl.Data(xmin,xmax,ymin,ymax)
 fmm = vgl.FrameManager()
-frm = fmm.create(0,0,3,3, data)
+frm = fmm.create(0,0,5,2, data)
 
+def write_tec():
+    fp = open("cycloid2d.tec", "wt")
+    
+    fp.write("variables = x,y\nzone t=\"cycloid\", i = %d\n"%x.size)
+    
+    for x1, y1 in zip(x,y):
+        fp.write("%f %f\n"%(x1, y1))
+    fp.close()
+    
 def save_cycloid_img():
-    dev = vgl.DeviceCairo("cycloid.jpg", fmm.get_gbbox(), 150)
+    dev = vgl.DeviceCairo("cycloid.jpg", fmm.get_gbbox(), 300)
     dev.set_device(frm)
+    vgl.draw_frame(dev, frm)
     plot_cycloid(dev)
     dev.close()
 
 def save_cycloid_mov():
     global dev
-    dev = vgl.DeviceCairo("", fmm.get_gbbox(), 150)
-    frm.xaxis.xlabel.size *= 2
-    frm.yaxis.ylabel.size *= 2
+    dev = vgl.DeviceCairo("", fmm.get_gbbox(), 300)
     dev.set_device(frm)
-    dev_mov = vgl.DeviceCairoAnimation("cycloid.mp4", dev, movie_cycloid, dur, fps = fps)
+    frm.xaxis.label.size *= 2
+    frm.yaxis.label.size *= 2
+    dev_mov = vgl.DeviceCairoAnimation("cycloid.mp4", dev, movie_cycloid, dur, fps)
     dev_mov.save_video()
 
-save_cycloid_img()
+#save_cycloid_img()
 save_cycloid_mov()
+#write_tec()
