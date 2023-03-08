91aa1679cf2bf5008348306685e33f3ac033b1dd
uhwang <43251090+uhwang@users.noreply.github.com>
2023-03-07 19:31:37 -0500

    Add files via upload

--- samples/circle.py
+++ samples/circle.py
@@ -15,24 +15,11 @@ x = rad*np.sin(theta)
 y = rad*np.cos(theta)
 
 def circle(dev):
-    vgl.drawgrid.draw_grid(dev)
-    vgl.drawlabel.draw_label(dev)
-    vgl.drawtick.draw_tick(dev)
-    dev.circle(4,0,rad,vgl.color.BLACK, 0.001*dev.frm.hgt())
-    dev.polyline(x, y, vgl.color.RED, 0.002*dev.frm.hgt())
-    dev.circle(0, 0, rad, vgl.color.BLUE, 0.001*dev.frm.hgt())
-    
-    x0=dev._x_viewport(0)
-    y0=dev._x_viewport(0)
-    x1=dev._x_viewport(x0-rad)
-    x2=dev._x_viewport(x0+rad)
-    y1=dev._y_viewport(y0-rad)
-    y2=dev._y_viewport(y0+rad)
-    xdif=(x2-x0)-(x0-x1)
-    ydif=(y2-y0)-(y0-y1)
-    print("xdif",xdif)
-    print("ydif",ydif)
-    
+    vgl.drawaxis.draw_axis(dev)
+    dev.circle(4,0,rad,vgl.color.BLACK, 0.003*dev.frm.hgt(), lpat=vgl.patline.get_stock_dash())
+    #dev.polyline(x, y, vgl.color.RED, 0.002*dev.frm.hgt())
+    dev.circle(0, 0, rad, vgl.color.BLUE, 0.003*dev.frm.hgt(), lpat=vgl.patline.get_stock_dashdot())
+   
 def main():
     dev_img = vgl.device.DeviceCairo("circle.jpg", gbox, 300)
     dev_img.set_plot(frm, extend=vgl.device._FIT_EXTEND_Y)
