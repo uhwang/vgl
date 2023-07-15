ffada56f09b4c15863b7b5f5693ddfaa0a471795
uhwang <43251090+uhwang@users.noreply.github.com>
2023-07-15 00:19:25 -0400

    Add files via upload

--- samples/test_arrow.py
+++ samples/test_arrow.py
@@ -9,7 +9,7 @@ fmm = vgl.FrameManager()
 frm = fmm.create(1,1,4,4, data)
 frm.show_all_major_grid()
 nth = 13
-r = 1
+r = 0.9
 th = np.linspace(0, np.pi*2, nth)
 ctbl = vgl.create_color_table(0,270, 0.8, 1, nth)
 
@@ -18,6 +18,7 @@ arrows = [vgl.basicshape.EndArrowLine(frm,
             r*np.cos(t),
             r*np.sin(t),
             lpat=vgl.linepat._PAT_DASH,
+            lthk = 0.005,
             col = ctbl[i],
             #type_t = vgl.basicshape._ARROWTYPE_OPEN) 
             #type_t = vgl.basicshape._ARROWTYPE_CLOSED) 
@@ -27,20 +28,28 @@ arrows = [vgl.basicshape.EndArrowLine(frm,
 del arrows[-1]
 
 arrow = vgl.basicshape.EndArrowLine(frm, 
-            0,0, 1, 1,
-            lpat=vgl.linepat._PAT_DASH,
-            col=vgl.color.RED,
-            type_t = vgl.basicshape._ARROWTYPE_CLOSEDFILLED) 
-            
+            1.5, 1.2, 3.5, 1.2,
+            #lpat=vgl.linepat._PAT_DASH,
+            lthk = 0.005,
+            col=vgl.color.GREEN,
+            type_t = vgl.basicshape._ARROWTYPE_CLOSED,
+            viewport=True) 
+
+box = vgl.basicshape.Box(-1,-1,0.25,0.25,
+                        lcol=vgl.color.BLUE,
+                        lthk=0.005,
+                        fcol=vgl.color.CUSTOM1)
 def plot(dev):
     vgl.draw_frame(dev)
     vgl.draw_axis(dev)
     
-    #arrow.draw(dev)
+    arrow.draw(dev)
     
     for a in arrows:
         a.draw(dev)
         
+    box.draw(dev)
+    
     dev.close()
     
 dev_img = vgl.DeviceCairo("test-arrow.jpg", fmm.get_gbbox(), 300)
