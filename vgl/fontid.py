79fb3027b686d3237c9d49ebbf572027cb990791
uhwang <43251090+uhwang@users.noreply.github.com>
2023-08-01 14:00:15 -0400

    Add files via upload

--- vgl/fontid.py
+++ vgl/fontid.py
@@ -29,14 +29,51 @@ FONT_GREEK               = 17
 FONT_GREEKCOMPLEX        = 18
 FONT_GREEKSIMPLEX        = 19
 FONT_MATHLOW             = 20
-FONT_MATHUPPER           = 31
-FONT_MATHSYMBOL          = 32
-FONT_SCRIPTCOMPLEX       = 33
-FONT_SCRIPTSIMPLEX       = 34
-FONT_SYMBOL              = 35
-FONT_MARKER              = 36
-FONT_ASTROLOGY           = 37
-FONT_CYILLIC             = 38
-FONT_CYILLIC1            = 39
+FONT_MATHUPPER           = 21
+FONT_MATHSYMBOL          = 22
+FONT_SCRIPTCOMPLEX       = 23
+FONT_SCRIPTSIMPLEX       = 24
+FONT_SYMBOL              = 25
+FONT_MARKER              = 26
+FONT_ASTROLOGY           = 27
+FONT_CYILLIC             = 28
+FONT_CYILLIC1            = 29
 FONT_METEOROLOGY         = 30
 FONT_MUSIC               = 31
+
+_FONT_LIST = [
+    FONT_ROMANSIMPLEX      ,
+    FONT_ROMANDUPLEX       ,
+    FONT_ROMANTRIPLEX      ,
+    FONT_CURSIVE           ,
+    FONT_FUTURAL           ,
+    FONT_FUTURAM           ,
+    FONT_TIMESROMAN        ,
+    FONT_TIMESROMANBOLD    ,
+    FONT_TIMESGREEK        ,
+    FONT_TIMESITALIAN      ,
+    FONT_TIMESITALIANBOLD  ,
+    FONT_GOTHENGTRIPLEX    ,
+    FONT_GOTHGERMANTRIPLEX ,
+    FONT_GOTHENGLISH       ,
+    FONT_GOTHGERMAN        ,
+    FONT_GOTHITALIAN       ,
+    FONT_GOTHITALIANTRIPLEX,
+    FONT_GREEK             ,
+    FONT_GREEKCOMPLEX      ,
+    FONT_GREEKSIMPLEX      ,
+    FONT_MATHLOW           ,
+    FONT_MATHUPPER         ,
+    FONT_MATHSYMBOL        ,
+    FONT_SCRIPTCOMPLEX     ,
+    FONT_SCRIPTSIMPLEX     ,
+    FONT_SYMBOL            ,
+    FONT_MARKER            ,
+    FONT_ASTROLOGY         ,
+    FONT_CYILLIC           ,
+    FONT_CYILLIC1          ,
+    FONT_METEOROLOGY       ,
+    FONT_MUSIC             
+]
+
+_valid_fid = lambda fid: fid >= FONT_ROMANSIMPLEX and fid <= FONT_MUSIC
