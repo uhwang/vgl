d60b44d60721d5b1a665042030571cc205234b4d
uhwang <43251090+uhwang@users.noreply.github.com>
2023-08-07 19:04:10 -0400

    Remove hgt

--- test/print_font.py
+++ test/print_font.py
@@ -2,11 +2,21 @@
 
 import vgl
 
-for fid in vgl.fontid._FONT_LIST:
-    vgl.print_hershey_font(fid, vgl.devutil._dev_img)    
-
-# print font with all formats
-#for fid in vgl.fontid._FONT_LIST:
-#    for div in vgl.devutil._dev_list:
-#        vgl.print_hershey_font(fid, div)
-#    
+def save():
+    import chkfld
+    
+    path = "./vgl-fonts"
+    chkfld.create_folder(path)
+    
+    for fid in vgl.fontid._FONT_LIST:
+        vgl.print_hershey_font(fid, vgl.devutil._dev_img, path)    
+    
+    # print font with all formats
+    #for fid in vgl.fontid._FONT_LIST:
+    #    for div in vgl.devutil._dev_list:
+    #        vgl.print_hershey_font(fid, div)
+    #    
+    
+    
+if __name__ == "__main__":
+    save()    
