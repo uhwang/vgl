Fix rotation

vgl/text.py
@@ -18,6 +18,9 @@ import vgl.fontid as fontid
 import vgl.fontm as fontm
 import vgl.symtbl as sym_search
 
+from . import util
+from . import vertex
+
 to_rad = 3.1415926535/180
 #void TG_TextOut(TG_Device_Ptr dev, TG_StrPtr str, TG_Float x, TG_Float y, TG_UShort align, TG_Float lthk, TG_Color fc, TG_Color lc)
 
@@ -50,20 +53,31 @@ IS_BOTTOM    = lambda a: (a)&TEXT_ALIGN_BOTTOM
 IS_VCENTER   = lambda a: (a)&TEXT_ALIGN_VCENTER
 
 class Font():
-    def __init__(self, fid=fontid.FONT_ROMANSIMPLEX, size=0.05,\
-                    lcol = color.BLACK, lthk=0.001, align = TEXT_ALIGN_BOTTOM):
+    def __init__(self, 
+                 fid      = fontid.FONT_ROMANSIMPLEX, 
+                 size     = 0.05,
+                 lcol     = color.BLACK, 
+                 lthk     = 0.001, 
+                 align    = TEXT_ALIGN_BOTTOM,
+                 show_box = False,
+                 fill_box = False,
+                 box_lcol = color.BLACK,
+                 box_lthk = 0.001,
+                 box_fcol = color.WHITE
+                ):
+                
         self.font_name = fontm.font_manager.get_font_name(fid)
         self.font_id   = fid
         self.size      = size
         self.lcol      = lcol
         self.lthk      = lthk
         self.align     = align
-        self.show_box  = False
-        self.fill_box  = False
-        self.box_lcol  = color.BLACK
-        self.box_fcol  = color.WHITE
-        self.box_lthk  = 0.001
-        
+        self.show_box  = show_box
+        self.fill_box  = fill_box
+        self.box_lcol  = box_lcol
+        self.box_lthk  = box_lthk
+        self.box_fcol  = box_fcol
+
     def set_font(self, fid):
         self.font_name = fontm.font_manager.get_font_name(fid)
         self.font_id   = fid
@@ -72,17 +86,51 @@ class Font():
     #def set_halign_center(self): self.align = 
 
 class Text(Font):
-    def __init__(self, x=0, y=0, text=''):
-        super().__init__()
+    def __init__(self, 
+                 x        = 0, 
+                 y        = 0, 
+                 text     = '', 
+                 deg      = 0,
+                 size     = 0.05,
+                 lcol     = color.BLACK, 
+                 lthk     = 0.001, 
+                 align    = TEXT_ALIGN_BOTTOM,
+                 show_box = False,
+                 fill_box = False,
+                 box_lcol = color.BLACK,
+                 box_lthk = 0.001,
+                 box_fcol = color.WHITE
+                ):
+        super().__init__( size     = size,
+                          lcol     = lcol,
+                          lthk     = lthk,
+                          align    = align,
+                          show_box = show_box,
+                          fill_box = fill_box,
+                          box_lcol = box_lcol,
+                          box_lthk = box_lthk,
+                          box_fcol = box_fcol)
         self.x     = x
         self.y     = y
         self.text  = text
-        self.rotation = 0
+        self.deg   = deg
 
-    def set_text(self, x, y, text):
-        self.x     = x
-        self.y     = y
-        self.text  = text
+    def __str__(self):
+        return "x   : %f\n"\
+               "y   : %f\n"\
+               "text: %s\n"\
+               "deg : %f\n"\
+               "lcol: %s\n"\
+               "lthk: %f\n"%\
+               (self.x, self.y, self.text, self.deg, str(self.lcol), self.lthk)
+               
+    def set_text(self, x, y, text, deg=0, lcol=color.BLACK, lthk=0.001):
+        self.x    = x
+        self.y    = y
+        self.text = text
+        self.deg  = deg
+        self.lcol = lcol
+        self.lthk = lthk
     '''
         E(ast) : LEFT
         W(est) : RIGHT
@@ -130,8 +178,6 @@ def write_text(dev, t, viewport=True):
     nstr=0 
     px=0 
     py=0
-    ccos = math.cos(t.rotation*to_rad)
-    csin = math.sin(t.rotation*to_rad)
     
     if viewport:
         curx=t.x 
@@ -152,8 +198,6 @@ def write_text(dev, t, viewport=True):
     max_x=0.0 
     delx =0.0 
     dely =0.0 
-    #xx=np.zeros(5)
-    #yy=np.zeros(5)
     ll=Point(0.0,0.0) 
     rt=Point(0.0,0.0)
     box = [Point]*5
@@ -207,45 +251,33 @@ def write_text(dev, t, viewport=True):
             
             if px==-1 and py==-1:
                 if xp and yp:
-                    llist.append((xp,yp))
+                    llist.append([xp,yp])
                 xp = []
                 yp = []
                 continue
             else:
                 cx = px*scale;
                 cy = (py+TEXT_DROP)*scale
-                #rx = cx*ccos-cy*csin
-                #ry = cx*csin+cy*ccos
-                #tx = curx+rx+scale*(chwid*0.5)*ccos
-                #ty = cury+ry
-                rx = cx
-                ry = cy#*y_reverse
-                tx = curx+rx+scale*(chwid*0.5)
-                ty = cury+ry
+                tx = curx+cx+scale*(chwid*0.5)
+                ty = cury+cy
                 if fbox.sy > ty: fbox.sy = ty
                 if fbox.ey < ty: fbox.ey = ty
                 xp.append(tx)
                 yp.append(ty)
         
         if len(xp) != 0:
-            llist.append((xp,yp))
+            llist.append([xp,yp])
         clist.append(llist)
         
-        chwid = 2 if chwid == 2 else chwid
-        #delx = chwid*ccos*SHIFT_FACTOR*scale
-        #dely = chwid*csin*SHIFT_FACTOR*scale
+        chwid = 2 if chwid == 2 or chwid == 0 else chwid
         delx = chwid*SHIFT_FACTOR*scale
         dely = 0
         curx += delx
         cury += dely
     
-    #el = clist[-1]
-    #for l in el: ex = max(l[0])
     fbox.sx = 0
-    #fbox.ex = ex
     fbox.ex = curx
     
-    #fbox.expand(min(fbox.hgt(), fbox.wid())*0.05)
     dx = 0
     dy = 0
     # default align is Left & Vcenter
@@ -256,17 +288,20 @@ def write_text(dev, t, viewport=True):
     if IS_TOP    (t.align): dy =  fhgt
     if IS_BOTTOM (t.align): dy = -fhgt
     
-    fthk = t.lthk#*dev.frm.hgt()
-    bthk = t.box_lthk#*dev.frm.hgt()
+    fthk = t.lthk
+    bthk = t.box_lthk
+    
+    bvtx = vertex.Vertex(5)
+    bvtx.set_vertex(0, fbox.sx, fbox.sy)
+    bvtx.set_vertex(1, fbox.sx, fbox.ey)
+    bvtx.set_vertex(2, fbox.ex, fbox.ey)
+    bvtx.set_vertex(3, fbox.ex, fbox.sy)
+    bvtx.set_vertex(4, fbox.sx, fbox.sy)
+    
     if t.show_box or t.fill_box:
-        if t.rotation != 0:
-            x1 = fbox.sx*ccos-fbox.sy*csin
-            y1 = fbox.sx*csin+fbox.sy*ccos
-            x2 = fbox.ex*ccos-fbox.ey*csin
-            y2 = fbox.ex*csin+fbox.ey*ccos
-            fbox.set_bbox(x1,y1,x2,y2)
-        fbox.transx(dx)
-        fbox.transy(dy)
+        if t.deg != 0:
+            bvtx.rotate(t.deg)
+        bvtx.trans(dx,dy)
         if viewport:
             curx = t.x
             cury = t.y
@@ -274,13 +309,18 @@ def write_text(dev, t, viewport=True):
             curx = dev._x_viewport(t.x)
             cury = dev._y_viewport(t.y)
             
-        #fbox.trans(t.x,t.y)
-        fbox.trans(curx,cury)
+        bvtx.trans(curx,cury)
         if t.fill_box:
-            dev.lpolygon(fbox.get_xs(), fbox.get_ys(), None, None, t.box_fcol)
+            dev.lpolygon(bvtx.get_xs(), bvtx.get_ys(), None, None, t.box_fcol)
         if t.show_box:
-            dev.lpolyline(fbox.get_xs(), fbox.get_ys(), t.box_lcol, bthk, True)
+            dev.lpolyline(bvtx.get_xs(), bvtx.get_ys(), t.box_lcol, bthk, True)
     
+    if t.deg != 0:
+        for ll in clist:
+            for ls in ll:
+                r = util.deg_rotation(ls[0], ls[1], t.deg)
+                ls[0], ls[1] = r[0], r[1]
+
     for ll in clist:
         for ls in ll:
             if viewport:
@@ -290,12 +330,6 @@ def write_text(dev, t, viewport=True):
                 xx = np.array([dev._x_viewport(x1) for x1 in ls[0]])
                 yy = np.array([dev._x_viewport(y1) for y1 in ls[1]])
             
-            if t.rotation != 0:
-                for i in range(xx.size):
-                    xs = xx[i]*ccos-yy[i]*csin
-                    ys = xx[i]*csin-yy[i]*ccos
-                    xx[i] = xs
-                    yy[i] = ys
             if viewport:
                 xx += dx + t.x
                 yy += dy + t.y