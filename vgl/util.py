22be0e79b25ea9fba42a33df487f3f8000d62456
uhwang <43251090+uhwang@users.noreply.github.com>
2023-08-11 23:55:59 -0400

    Add rot matrix

--- vgl/util.py
+++ vgl/util.py
@@ -9,18 +9,34 @@ vgl_rtd= 57.29577951308233
 deg_to_rad = lambda d: d*vgl_dtr
 rad_to_deg = lambda r: r*vgl_rtd
 
-# angle in degree
-def rotation(x,y,angle,rad=True):
+def rot_matrix(angle, rad=True):
+
     if rad:
         c, s = np.cos(angle), np.sin(angle)
     else:
         c, s = np.cos(vgl_dtr*angle), np.sin(vgl_dtr*angle)    
         
-    R = np.array(((c, -s), (s, c)))
-    return np.matmul(R,[x,y])
+    return np.array([[c, -s], [s, c]])
+    
+def deg_rot_matrix(angle):
+
+    return rot_matrix(angle, rad=False)
+    
+def rad_rot_matrix(angle):
+
+    return rot_matrix(angle)
+    
+
+# angle in degree
+def rotation(x,y,angle,rad=True):
+
+    return np.matmul(rot_matrix(angle, rad),[x,y])
         
 def deg_rotation(x, y, angle):
+
     return rotation(x,y,angle,rad=False)
     
 def rad_rotation(x, y, angle):
+
     return rotation(x,y,angle)
+
